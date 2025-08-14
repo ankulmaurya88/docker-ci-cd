dc4f2d8dbe3d5f246d6b661b97c177de8e6339f5
ankulmaurya88 <ankulmaurya88@gmail.com>
2025-08-14 08:17:53 +0530

    ci-cd with docker

--- app/app/main.py
+++ app/app/main.py
@@ -1,4 +1,4 @@
-from app.math_ops import add
+from math_ops import add
 
 def greet(name):
     return f"Hello, {name}! 2+3={add(2,3)}"
--- tests/test_main.py
+++
@@ -1,4 +0,0 @@
-from app.main import greet
-
-def test_greet():
-    assert "Hello" in greet("Alice")
